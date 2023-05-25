1616c87938edac5575ccdfa3984c7d361c31d98a
Valentin Silvestre <vsilvestre.pro@gmail.com>
2023-05-25 11:43:32 +0200

    Better alert

--- app/page.tsx
+++ app/page.tsx
@@ -253,27 +253,26 @@ export default function Home() {
             </div>
             {alert && (
                 <div
-                    className={
-                        'flex sticky top-[100vh] items-center justify-center p-4 mb-auto'
-                    }
-                >
-                    <div
-                        role="alert"
-                        className="rounded border-s-4 border-red-500 bg-red-50 p-4 sticky top-[100vh]"
-                    >
-                        <strong className="block font-medium text-red-800">
-                            {alert.message}
-                        </strong>
-                        <div className="mt-2 text-sm text-red-700">
-                            <ul>
-                                {alert.data &&
-                                    alert.data.map(
-                                        (data: string, index: number) => (
-                                            <li key={index}>{data}</li>
-                                        )
-                                    )}
-                            </ul>
-                        </div>
+                    className="fixed bottom-5 flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
+                    role="alert">
+                    <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor"
+                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                              clipRule="evenodd"></path>
+                    </svg>
+                    <span className="sr-only">{alert.message}</span>
+                    <div>
+                        <span className="font-medium">Ensure that these requirements are met:</span>
+                        <ul className="mt-1.5 ml-4 list-disc list-inside">
+                            {alert.data &&
+                                alert.data.map(
+                                    (data: string, index: number) => (
+                                        <li key={index}>{data}</li>
+                                    )
+                                )
+                            }
+                        </ul>
                     </div>
                 </div>
             )}
