4e688cb259cbd33035806b07096374ac4efb6651
Valentin Silvestre <vsilvestre.pro@gmail.com>
2023-06-01 09:19:59 +0200

    Release 0.8.3
    Release pote

--- app/(handler)/fetchGithubUpdatedMods.tsx
+++ app/(handler)/fetchGithubUpdatedMods.tsx
@@ -21,9 +21,21 @@ export async function fetchGithubUpdatedMods(
         },
     })
 
-    const zipAsset = mods.data.assets.find((asset) => {
+    let zipAsset = mods.data.assets.find((asset) => {
         return asset.name.endsWith('_full.zip')
     })
+
+    // Backward compatibility
+    if (!zipAsset) {
+        zipAsset = mods.data.assets.find((asset) => {
+            return asset.name.startsWith('TOTK-Mods-collection') && asset.name.endsWith('.zip')
+        })
+    }
+    if (!zipAsset) {
+        zipAsset = mods.data.assets.find((asset) => {
+            return asset.name.endsWith('.zip')
+        })
+    }
 
     if (!zipAsset) {
         throw new Error('No zip asset found')
